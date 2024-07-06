61acd0905d44fb84a589644f2b4abcbe568d52a1
Parthavee <naikparthavee06@gmail.com>
2024-07-06 03:34:17 -0400

    Reached Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,36 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  res.send(JSON.stringify(books, null, 4));
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+
+  res.send(JSON.stringify(book, null, 4));
+
+  
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  let author = req.params.author;
+
+  let book = books.filter((item) => { return item.author === author});
+
+  if(book.length > 0){
+    res.send(JSON.stringify(book, null, 4));
+  }
+  
+
 });
 
 // Get all books based on title
