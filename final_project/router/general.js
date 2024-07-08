477c56c97995d8d8646470e0a09896a889f7c52f
Parthavee <naikparthavee06@gmail.com>
2024-07-08 14:19:48 -0400

    Reached task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,24 +24,37 @@ public_users.post("/register", (req,res) => {
 
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  
-  res.send(JSON.stringify(books, null, 4));
+const getBooks = () => {
+    return books;
+}
 
+// Get the book list available in the shop
+public_users.get('/', async function (req, res) {
+  //Write your code here
+
+  try{
+    const bookList = await getBooks();
+    res.status(200).send(JSON.stringify(books, null, 4));
+
+  } catch(err) {
+    res.status(400).json({ message: err });
+  }
+  
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
 
   const isbn = req.params.isbn;
-  const book = books[isbn];
+  try {
+    const book = await books[isbn];
+    console.log("Reached isbn" , isbn);
+    res.send(JSON.stringify("book", null, 4));
 
-  res.send(JSON.stringify(book, null, 4));
-
-  
+  } catch(err) {
+    res.status(400).json({ message: err });
+  }
  });
   
 // Get book details based on author
